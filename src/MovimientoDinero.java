01fc262430f5de18f91db62a0b963ccbca94bf29
kikejaimes <kikejaimes0705@gmail.com>
2022-08-23 18:22:15 -0500

    Subiendo clase

---
+++ src/MovimientoDinero.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+    public class MovimientoDinero {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            String usuario;
+            double montoMovimiento;
+
+            System.out.println("Ingrese el nombre de usuario");
+            usuario = sc.nextLine();
+
+            System.out.println("Ingrese saldo de cuenta");
+            montoMovimiento = sc.nextDouble();
+
+
+            System.out.println("el usuario " + usuario + "realizo un moviento de "+ montoMovimiento);
+        }
+
+    }
+
+
+
+
+
+}
