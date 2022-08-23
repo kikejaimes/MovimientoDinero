97971d0762fec90e96fbc235dc68235a5e648d33
kikejaimes <kikejaimes0705@gmail.com>
2022-08-23 18:59:57 -0500

    actualizando clase

--- src/MovimientoDinero.java
+++ src/MovimientoDinero.java
@@ -1,28 +1,43 @@
-import java.util.Scanner;
+public class MovimientoDinero {
 
-    public class MovimientoDinero {
+    private String usuario;
+    private Double movimientoD;
 
-        public static void main(String[] args) {
-
-            Scanner sc = new Scanner(System.in);
-
-            String usuario;
-            double montoMovimiento;
-
-            System.out.println("Ingrese el nombre de usuario");
-            usuario = sc.nextLine();
-
-            System.out.println("Ingrese saldo de cuenta");
-            montoMovimiento = sc.nextDouble();
-
-
-            System.out.println("el usuario " + usuario + "realizo un moviento de "+ montoMovimiento);
-        }
-
+    public MovimientoDinero(String usuario) {
+        this.usuario = usuario;
     }
 
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Double getMovimientoD() {
+        return movimientoD;
+    }
+
+    public void setMovimientoD(Double movimientoD) {
+        this.movimientoD = movimientoD;
+    }
+
+    public MovimientoDinero(String usuario, Double movimientoD) {
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
