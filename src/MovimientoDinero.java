84e48f60f9262853e8dbe381d01e200ecb46f304
kikejaimes <kikejaimes0705@gmail.com>
2022-08-23 19:24:15 -0500

    Clase M_Dinero actualizada

--- src/MovimientoDinero.java
+++ src/MovimientoDinero.java
@@ -1,10 +1,49 @@
 public class MovimientoDinero {
 
+    private Double montoMovimiento;
+    private Double montosPositivos;
+    private Double montosNegativos;
+    private String conceptoMovimiento;
     private String usuario;
-    private Double movimientoD;
 
-    public MovimientoDinero(String usuario) {
+    public MovimientoDinero(Double montoMovimiento, Double montosPositivos, Double montosNegativos, String conceptoMovimiento, String usuario) {
+        this.montoMovimiento = montoMovimiento;
+        this.montosPositivos = montosPositivos;
+        this.montosNegativos = montosNegativos;
+        this.conceptoMovimiento = conceptoMovimiento;
         this.usuario = usuario;
+    }
+
+    public Double getMontoMovimiento() {
+        return montoMovimiento;
+    }
+
+    public void setMontoMovimiento(Double montoMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+    }
+
+    public Double getMontosPositivos() {
+        return montosPositivos;
+    }
+
+    public void setMontosPositivos(Double montosPositivos) {
+        this.montosPositivos = montosPositivos;
+    }
+
+    public Double getMontosNegativos() {
+        return montosNegativos;
+    }
+
+    public void setMontosNegativos(Double montosNegativos) {
+        this.montosNegativos = montosNegativos;
+    }
+
+    public String getConceptoMovimiento() {
+        return conceptoMovimiento;
+    }
+
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+        this.conceptoMovimiento = conceptoMovimiento;
     }
 
     public String getUsuario() {
@@ -13,31 +52,5 @@ public class MovimientoDinero {
 
     public void setUsuario(String usuario) {
         this.usuario = usuario;
-    }
-
-    public Double getMovimientoD() {
-        return movimientoD;
-    }
-
-    public void setMovimientoD(Double movimientoD) {
-        this.movimientoD = movimientoD;
-    }
-
-    public MovimientoDinero(String usuario, Double movimientoD) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
